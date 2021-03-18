92aeab4fa968fdaa0e0c1aedfca92010b123a4f1
Mohammed Jaber <mohjbr.dev@gmail.com>
2021-03-18 17:53:51 +0200

    remove registration and forgotPass components

--- src/app/modules/Auth/pages/AuthPage.js
+++ src/app/modules/Auth/pages/AuthPage.js
@@ -4,8 +4,6 @@ import { Link, Switch, Redirect } from "react-router-dom";
 import { toAbsoluteUrl } from "../../../../_metronic/_helpers";
 import { ContentRoute } from "../../../../_metronic/layout";
 import Login from "./Login";
-import Registration from "./Registration";
-import ForgotPassword from "./ForgotPassword";
 import "../../../../_metronic/_assets/sass/pages/login/classic/login-1.scss";
 
 export function AuthPage() {
