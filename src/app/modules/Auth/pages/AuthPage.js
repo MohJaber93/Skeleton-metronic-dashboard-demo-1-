3164c8748bd0e4aa1bf53e81902c51132219846e
Mohammed Jaber <mohjbr.dev@gmail.com>
2021-03-18 17:51:48 +0200

    just use login page within AuthPages

--- src/app/modules/Auth/pages/AuthPage.js
+++ src/app/modules/Auth/pages/AuthPage.js
@@ -21,7 +21,7 @@ export function AuthPage() {
           <div
             className="login-aside d-flex flex-row-auto bgi-size-cover bgi-no-repeat p-10 p-lg-10"
             style={{
-              backgroundImage: `url(${toAbsoluteUrl("/media/bg/bg-4.jpg")})`,
+              backgroundImage: `url(${toAbsoluteUrl("/media/bg/bg-1.jpg")})`,
             }}
           >
             {/*begin: Aside Container*/}
@@ -47,25 +47,6 @@ export function AuthPage() {
                 </p>
               </div>
               {/* end:: Aside content */}
-
-              {/* start:: Aside footer for desktop */}
-              <div className="d-none flex-column-auto d-lg-flex justify-content-between mt-10">
-                <div className="opacity-70 font-weight-bold	text-white">
-                  &copy; 2020 Metronic
-                </div>
-                <div className="d-flex">
-                  <Link to="/terms" className="text-white">
-                    Privacy
-                  </Link>
-                  <Link to="/terms" className="text-white ml-10">
-                    Legal
-                  </Link>
-                  <Link to="/terms" className="text-white ml-10">
-                    Contact
-                  </Link>
-                </div>
-              </div>
-              {/* end:: Aside footer for desktop */}
             </div>
             {/*end: Aside Container*/}
           </div>
@@ -73,63 +54,16 @@ export function AuthPage() {
 
           {/*begin::Content*/}
           <div className="d-flex flex-column flex-row-fluid position-relative p-7 overflow-hidden">
-            {/*begin::Content header*/}
-            <div className="position-absolute top-0 right-0 text-right mt-5 mb-15 mb-lg-0 flex-column-auto justify-content-center py-5 px-10">
-              <span className="font-weight-bold text-dark-50">
-                Don't have an account yet?
-              </span>
-              <Link
-                to="/auth/registration"
-                className="font-weight-bold ml-2"
-                id="kt_login_signup"
-              >
-                Sign Up!
-              </Link>
-            </div>
-            {/*end::Content header*/}
-
             {/* begin::Content body */}
             <div className="d-flex flex-column-fluid flex-center mt-30 mt-lg-0">
               <Switch>
                 <ContentRoute path="/auth/login" component={Login} />
-                <ContentRoute
-                  path="/auth/registration"
-                  component={Registration}
-                />
-                <ContentRoute
-                  path="/auth/forgot-password"
-                  component={ForgotPassword}
-                />
                 <Redirect from="/auth" exact={true} to="/auth/login" />
                 <Redirect to="/auth/login" />
               </Switch>
             </div>
             {/*end::Content body*/}
 
-            {/* begin::Mobile footer */}
-            <div className="d-flex d-lg-none flex-column-auto flex-column flex-sm-row justify-content-between align-items-center mt-5 p-5">
-              <div className="text-dark-50 font-weight-bold order-2 order-sm-1 my-2">
-                &copy; 2020 Metronic
-              </div>
-              <div className="d-flex order-1 order-sm-2 my-2">
-                <Link to="/terms" className="text-dark-75 text-hover-primary">
-                  Privacy
-                </Link>
-                <Link
-                  to="/terms"
-                  className="text-dark-75 text-hover-primary ml-4"
-                >
-                  Legal
-                </Link>
-                <Link
-                  to="/terms"
-                  className="text-dark-75 text-hover-primary ml-4"
-                >
-                  Contact
-                </Link>
-              </div>
-            </div>
-            {/* end::Mobile footer */}
           </div>
           {/*end::Content*/}
         </div>
