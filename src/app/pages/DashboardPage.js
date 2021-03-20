fdc938ffb04a34336cf5415728c0078f42fe2c3a
Mohammed Jaber <mohjbr.dev@gmail.com>
2021-03-20 21:28:53 +0200

    layout dashboard page

--- src/app/pages/DashboardPage.js
+++ src/app/pages/DashboardPage.js
@@ -1,5 +1,14 @@
 import React from "react";
+import { Grid, Typography, Box } from "@material-ui/core";
+
+import SummarySection from "../../_metronic/layout/components/SummarySection";
+import SummaryDetails from "../../_metronic/layout/components/SummaryDetails";
 
 export function DashboardPage() {
-  return <></>;
+  return (
+    <Box height="100%">
+      <SummarySection />
+      <SummaryDetails />
+    </Box>
+  );
 }
