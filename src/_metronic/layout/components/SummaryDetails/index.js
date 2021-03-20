b2b2f0d4cbfcd7e620c480a75276c41252320fae
Mohammed Jaber <mohjbr.dev@gmail.com>
2021-03-20 21:27:53 +0200

    add summary details component

---
+++ src/_metronic/layout/components/SummaryDetails/index.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { Grid, Typography } from "@material-ui/core";
+import SummaryTable from "../SummaryTable";
+import { useStyles } from "./style";
+
+const SummaryDetails = () => {
+  const classes = useStyles();
+
+  return (
+    <Grid container className={classes.summaryDetailsContainer}>
+      <Grid item sm={6} style={{ padding: "5px", width: "100%" }}>
+        <Typography variant="h5" gutterBottom>
+          اليوم
+        </Typography>
+        <Typography variant="h5" gutterBottom>
+          {new Date().toDateString()}
+        </Typography>
+        <SummaryTable />
+      </Grid>
+      <Grid item sm={6} style={{ padding: "5px", width: "100%" }}>
+        <Typography variant="h5" gutterBottom>
+          هذا الأسبوع
+        </Typography>
+        <Typography variant="h5" gutterBottom>
+          {new Date().toDateString()}
+        </Typography>
+        <SummaryTable />
+      </Grid>
+    </Grid>
+  );
+};
+
+export default SummaryDetails;
---
+++ src/_metronic/layout/components/SummaryDetails/style.js
@@ -0,0 +1,8 @@
+import makeStyles from "@material-ui/core/styles/makeStyles";
+
+export const useStyles = makeStyles(theme => ({
+  summaryDetailsContainer: {
+    minHeight: "400px",
+    marginTop: "30px"
+  }
+}));
