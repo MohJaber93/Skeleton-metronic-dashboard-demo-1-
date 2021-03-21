eaa9bd8c3e9ae415daaf6ef980d8e7a65fcb6bb9
Mohammed Jaber <mohjbr.dev@gmail.com>
2021-03-21 12:49:24 +0200

    create login api function

---
+++ src/api/Auth/index.js
@@ -0,0 +1,34 @@
+import { API_END_POINT } from "config";
+import { API_COMMON_STATUS, getUnknownStatusError } from "helpers/api-helper";
+import axios from "axios";
+
+export const login = async loginData => {
+  try {
+    const response = await axios.post(
+      `${API_END_POINT}/admin/login`,
+      loginData
+    );
+    console.log(response, "login response");
+    let data = {};
+    switch (response.status) {
+      case API_COMMON_STATUS.SUCCESS:
+        data = {
+          responseStatus: API_COMMON_STATUS.SUCCESS,
+          ...response.data
+        };
+        break;
+      case API_COMMON_STATUS.ERROR:
+        data = {
+          responseStatus: API_COMMON_STATUS.ERROR,
+          message: response.message
+        };
+        break;
+      default:
+        data = getUnknownStatusError();
+    }
+    return data;
+  } catch (error) {
+    console.log(error, error.message, error.name);
+    console.error(error);
+  }
+};
