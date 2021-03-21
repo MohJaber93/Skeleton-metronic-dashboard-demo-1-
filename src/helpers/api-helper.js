f3efb8049338cea2824de2a5696c036657fec4d1
Mohammed Jaber <mohjbr.dev@gmail.com>
2021-03-21 12:51:19 +0200

    add custom api helpers

---
+++ src/helpers/api-helper.js
@@ -0,0 +1,29 @@
+/**
+ * This is API common status RESPONSE
+ *
+ */
+const API_COMMON_STATUS = Object.freeze({
+  SUCCESS: 200,
+  FAILED: 500,
+  VALIDATION_ERROR: 422,
+  RESOURCE_CREATED: 201,
+  PUT_SUCCESS: 202,
+  RESOURCE_DELETED: 204,
+  UNAUTHORIZED: 401,
+  UNAUTHENTICATED: 403,
+  ERROR: 404
+});
+
+/**
+ * This method will return common status error for unkown status
+ *
+ * @returns {{message: string, responseStatus: number}}
+ */
+const getUnknownStatusError = () => {
+  return {
+    responseStatus: API_COMMON_STATUS.ERROR,
+    message: "Unknown Error Status!"
+  };
+};
+
+export { API_COMMON_STATUS, getUnknownStatusError };
