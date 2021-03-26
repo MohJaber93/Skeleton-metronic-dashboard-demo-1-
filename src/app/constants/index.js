a4317f78895d9bfcb9ffb32f90344e93ff25b6a4
Mohammed Jaber <mohjbr.dev@gmail.com>
2021-03-26 20:07:10 +0200

    move constants in app folder

---
+++ src/app/constants/index.js
@@ -0,0 +1,10 @@
+export const GLOBALSTATE_ACTIONS = {
+  SET_IS_AUTH: "SET_IS_AUTH",
+  LOGOUT: "LOGOUT"
+};
+
+export const ORDERS_STATUS = {
+  pendingOrders: "الطلبات المعلقة",
+  deliveredOrders: "الطلبات المستلمة",
+  rejectedOrders: "الطلبات المرفوضة"
+};
--- src/constants/index.js
+++
@@ -1,4 +0,0 @@
-export const GLOBALSTATE_ACTIONS = {
-  SET_IS_AUTH: "SET_IS_AUTH",
-  LOGOUT: "LOGOUT"
-};
