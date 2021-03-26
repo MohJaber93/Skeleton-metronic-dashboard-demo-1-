814574e5003ccf2e81b6dc8cc794d8a267f4e7e1
Mohammed Jaber <mohjbr.dev@gmail.com>
2021-03-26 16:20:31 +0200

    remove unused code

--- src/app/modules/Auth/pages/Login.js
+++ src/app/modules/Auth/pages/Login.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { useFormik } from "formik";
 import * as Yup from "yup";
 import { FormattedMessage, injectIntl } from "react-intl";
