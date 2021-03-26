de7a9d71aa960e93a2379197d1a5457e9a094f38
Mohammed Jaber <mohjbr.dev@gmail.com>
2021-03-26 21:39:20 +0200

    render orders page

--- src/app/BasePage.js
+++ src/app/BasePage.js
@@ -1,23 +1,25 @@
-import React, {Suspense} from "react";
-import {Redirect, Switch} from "react-router-dom";
-import {LayoutSplashScreen, ContentRoute} from "../_metronic/layout";
-import {BuilderPage} from "./pages/BuilderPage";
-import {MyPage} from "./pages/MyPage";
-import {DashboardPage} from "./pages/DashboardPage";
+import React, { Suspense } from "react";
+import { Redirect, Switch } from "react-router-dom";
+import { LayoutSplashScreen, ContentRoute } from "../_metronic/layout";
+import { BuilderPage } from "./pages/BuilderPage";
+import { MyPage } from "./pages/MyPage";
+import Orders from "./pages/Orders";
+import { DashboardPage } from "./pages/DashboardPage";
 
 export default function BasePage() {
-     return (
-      <Suspense fallback={<LayoutSplashScreen />}>
-       <Switch>
-         {
+  return (
+    <Suspense fallback={<LayoutSplashScreen />}>
+      <Switch>
+        {
           /* Redirect from root URL to /dashboard. */
-          <Redirect exact from="/" to="/dashboard"/>
-         }
-         <ContentRoute path="/dashboard" component={DashboardPage} />
-         <ContentRoute path="/builder" component={BuilderPage} />
-         <ContentRoute path="/my-page" component={MyPage} />
-         <Redirect to="error/error-v1" />
-       </Switch>
-     </Suspense>
+          <Redirect exact from="/" to="/dashboard" />
+        }
+        <ContentRoute path="/dashboard" component={DashboardPage} />
+        <ContentRoute path="/builder" component={BuilderPage} />
+        <ContentRoute path="/orders" component={Orders} />
+        <ContentRoute path="/my-page" component={MyPage} />
+        <Redirect to="error/error-v1" />
+      </Switch>
+    </Suspense>
   );
 }
