5b1771ad91eb6327204fc8f1b72d3b18af0b88e7
Mohammed Jaber <mohjbr.dev@gmail.com>
2021-03-27 22:56:38 +0300

    move Orders page into orders folder

--- src/app/pages/Orders/index.js
+++ src/app/pages/Orders/index.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { Box, Grid } from "@material-ui/core";
 import OrdersFilterControls from "_metronic/layout/components/OrdersFilterControls";
+import OrdersCards from "_metronic/layout/components/OrdersCards";
 
 const Orders = () => {
   return (
     <Box height="100%" style={{ backgroundColor: "#fff", padding: "5px" }}>
       <OrdersFilterControls />
       {/* My Page */}
+      <OrdersCards />
     </Box>
   );
 };
