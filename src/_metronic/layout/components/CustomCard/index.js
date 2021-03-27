39a201868dd5abdda952ff8f068796b46af415a8
Mohammed Jaber <mohjbr.dev@gmail.com>
2021-03-27 22:55:33 +0300

    adjust card component to be reusable

--- src/_metronic/layout/components/CustomCard/index.js
+++ src/_metronic/layout/components/CustomCard/index.js
@@ -2,7 +2,14 @@ import React from "react";
 import { Paper, Typography, Box } from "@material-ui/core";
 import { useStyles } from "./style";
 
-const CustomCard = ({ title, value, styleClass, icon: Icon, ...rest }) => {
+const CustomCard = ({
+  title,
+  value,
+  styleClass,
+  icon: Icon,
+  children,
+  ...rest
+}) => {
   const classes = useStyles();
   return (
     <Paper
@@ -10,24 +17,30 @@ const CustomCard = ({ title, value, styleClass, icon: Icon, ...rest }) => {
       className={`${classes.cardContainer} ${styleClass}`}
       {...rest}
     >
-      <Box
-        height="100%"
-        padding="5px 20px"
-        display="flex"
-        flexDirection="column"
-        justifyContent="center"
-      >
-        <Typography
-          className={classes.cardTitle}
-          color="inherit"
-          variant="h5"
-          gutterBottom
-        >
-          {title}
-        </Typography>
-        <Typography variant="h6">{value}</Typography>
-      </Box>
-      <Icon className={classes.cardIcon} fontSize="large" />
+      {Icon ? (
+        <>
+          <Box
+            height="100%"
+            padding="5px 20px"
+            display="flex"
+            flexDirection="column"
+            justifyContent="center"
+          >
+            <Typography
+              className={classes.cardTitle}
+              color="inherit"
+              variant="h5"
+              gutterBottom
+            >
+              {title}
+            </Typography>
+            <Typography variant="h6">{value}</Typography>
+          </Box>
+          <Icon className={classes.cardIcon} fontSize="large" />
+        </>
+      ) : (
+        children
+      )}
     </Paper>
   );
 };
