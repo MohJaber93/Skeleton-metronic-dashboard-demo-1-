e3728a14e2acf598b3c849b7f28570361cb9bb33
Mohammed Jaber <mohjbr.dev@gmail.com>
2021-04-02 19:29:05 +0300

    render ordersCards data from api

--- src/_metronic/layout/components/OrdersCards/index.js
+++ src/_metronic/layout/components/OrdersCards/index.js
@@ -1,73 +1,69 @@
-import React from "react";
+import React, { useMemo } from "react";
 import { Typography, Grid } from "@material-ui/core";
 import CustomCard from "../CustomCard";
 import { useStyles } from "./style";
 
-const OrdersCards = () => {
+const OrdersCards = ({ data }) => {
   const classes = useStyles();
+
+  const CARDS_DATA = useMemo(
+    () => [
+      {
+        title: "الطلبات المعلقة",
+        value: data.pendingOrders,
+        className: classes.pendingCard
+      },
+      {
+        title: "الطلبات المقبولة",
+        value: data.acceptedOrders,
+        className: classes.acceptedCard
+      },
+      {
+        title: "الطلبات المستلمة",
+        value: data.deliveredOrders,
+        className: classes.deliveredCard
+      },
+      {
+        title: "الطلبات المرفوضة",
+        value: data.rejectedOrders,
+        className: classes.rejectedCard
+      },
+      {
+        title: "طلبات أخرى",
+        value: data.othersOrder,
+        className: classes.othersCard
+      },
+      {
+        title: "كل الطلبات",
+        value: data.totalOrders,
+        className: classes.totalCard
+      }
+    ],
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [data]
+  );
+
   return (
     <CustomCard styleClass={classes.cardsSection}>
       <CustomCard>
         <CustomCard>
           <Grid container>
-            <Grid
-              item
-              container
-              direction="column"
-              justify="space-between"
-              alignItems="center"
-              xs={6}
-              md={3}
-            >
-              <CustomCard className={`${classes.card} ${classes.pendingCard}`}>
-                <Typography variant="h5">10000000000</Typography>
-              </CustomCard>
-              <Typography variant="h6">الطلبات المعلقة</Typography>
-            </Grid>
-            <Grid
-              item
-              container
-              direction="column"
-              justify="space-between"
-              alignItems="center"
-              xs={6}
-              md={3}
-            >
-              <CustomCard
-                className={`${classes.card} ${classes.deliveredCard}`}
+            {CARDS_DATA.map(card => (
+              <Grid
+                item
+                container
+                direction="column"
+                justify="space-between"
+                alignItems="center"
+                xs={4}
+                md={2}
               >
-                <Typography variant="h5">2</Typography>
-              </CustomCard>
-              <Typography variant="h6">الطلبات المستلمة</Typography>
-            </Grid>
-            <Grid
-              item
-              container
-              direction="column"
-              justify="space-between"
-              alignItems="center"
-              xs={6}
-              md={3}
-            >
-              <CustomCard className={`${classes.card} ${classes.rejectedCard}`}>
-                <Typography variant="h5">3</Typography>
-              </CustomCard>
-              <Typography variant="h6">الطلبات المرفوضة</Typography>
-            </Grid>
-            <Grid
-              item
-              container
-              direction="column"
-              justify="space-between"
-              alignItems="center"
-              xs={6}
-              md={3}
-            >
-              <CustomCard className={`${classes.card} ${classes.totalCard}`}>
-                <Typography variant="h5">4</Typography>
-              </CustomCard>
-              <Typography variant="h6">كل الطلبات</Typography>
-            </Grid>
+                <CustomCard className={`${classes.card} ${card.className}`}>
+                  <Typography variant="h5">{card.value}</Typography>
+                </CustomCard>
+                <Typography variant="h6">{card.title}</Typography>
+              </Grid>
+            ))}
           </Grid>
         </CustomCard>
       </CustomCard>
