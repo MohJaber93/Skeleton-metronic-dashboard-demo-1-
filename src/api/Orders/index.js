f0d7589b9586349e4f141e3d921b46906065f991
Mohammed Jaber <mohjbr.dev@gmail.com>
2021-04-02 19:28:00 +0300

    add get all orders api function

---
+++ src/api/Orders/index.js
@@ -0,0 +1,39 @@
+import { API_COMMON_STATUS, getUnknownStatusError } from "helpers/api-helper";
+import axios from "axios";
+
+export const getOrders = async token => {
+  try {
+    const response = await axios.get(`/orders/all`, {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    });
+    let data = {};
+    switch (response.status) {
+      case API_COMMON_STATUS.SUCCESS:
+        data = {
+          responseStatus: API_COMMON_STATUS.SUCCESS,
+          ...response.data
+        };
+        break;
+      case API_COMMON_STATUS.UNAUTHORIZED:
+        data = {
+          responseStatus: API_COMMON_STATUS.UNAUTHORIZED,
+          message: response.data.error
+        };
+        break;
+      case API_COMMON_STATUS.ERROR:
+        data = {
+          responseStatus: API_COMMON_STATUS.ERROR,
+          message: response.data.message
+        };
+        break;
+      default:
+        data = getUnknownStatusError();
+    }
+    return data;
+  } catch (error) {
+    console.log(error, error.message, error.name);
+    console.error(error);
+  }
+};
