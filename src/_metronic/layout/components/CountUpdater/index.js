8564a654e49f42d8327366beef04d3da5343cc9b
Mohammed Jaber <mohjbr.dev@gmail.com>
2021-04-03 17:50:25 +0300

    create counterUpdater component

---
+++ src/_metronic/layout/components/CountUpdater/index.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { IconButton, Tooltip } from "@material-ui/core";
+import TimerIcon from "@material-ui/icons/Timer";
+
+const CountUpdater = ({ startNumber, onCounterFinished }) => {
+  const [count, setCount] = React.useState(startNumber);
+  React.useEffect(() => {
+    console.log("count", count);
+    let timer;
+    if (count !== 0) {
+      timer = setTimeout(() => {
+        setCount(prevCount => prevCount - 1);
+      }, 1000);
+    } else {
+      setCount(startNumber);
+      onCounterFinished();
+    }
+
+    return () => {
+      clearTimeout(timer);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [count, startNumber]);
+  return (
+    <Tooltip title="تحديث">
+      <IconButton
+        color="primary"
+        onClick={() => {
+          onCounterFinished();
+          setCount(startNumber);
+        }}
+      >
+        {count}
+        <TimerIcon />
+      </IconButton>
+    </Tooltip>
+  );
+};
+
+export default CountUpdater;
