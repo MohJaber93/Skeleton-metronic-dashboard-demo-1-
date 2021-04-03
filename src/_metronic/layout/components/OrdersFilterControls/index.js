1d388632526117c446a073db10d0c31a5d8b7747
Mohammed Jaber <mohjbr.dev@gmail.com>
2021-04-03 23:10:09 +0300

    handle filterData state

--- src/_metronic/layout/components/OrdersFilterControls/index.js
+++ src/_metronic/layout/components/OrdersFilterControls/index.js
@@ -5,18 +5,37 @@ import RotateLeftIcon from "@material-ui/icons/RotateLeft";
 import CustomInput from "../CustomInput";
 import CustomSelect from "../CustomSelect";
 import CustomButton from "../CustomButton";
-import { ORDERS_STATUS } from "app/constants";
+import { ORDERS_STATUS, ORDERS_DELIVERY_METHOD } from "app/constants";
 import { useStyles } from "./style";
 
 const OrdersFilterControls = () => {
+  const [filterData, setFilterData] = React.useState({
+    orderNumber: "",
+    deliveryMethod: "الكل",
+    orderStatus: "الكل",
+    startDate: "", // .replaceAll('-','/')
+    endDate: ""
+  });
   const classes = useStyles();
+
+  const changeHandler = event => {
+    event.persist();
+    console.log("event.target.value", event.target.value);
+    setFilterData(prevFilterData => ({
+      ...prevFilterData,
+      [event.target.name]: event.target.value
+    }));
+  };
 
   return (
     <Grid container spacing={2} className={classes.ordersFilterContainer}>
-      <Grid item md={3} sm={3} xs={12}>
+      <Grid item md={2} sm={4} xs={12}>
         <CustomInput
           type="number"
           placeholder="رقم الطلب"
+          name="orderNumber"
+          value={filterData.orderNumber}
+          onChange={changeHandler}
           inputProps={{
             min: 1
           }}
@@ -31,22 +50,45 @@ const OrdersFilterControls = () => {
           }}
         />
       </Grid>
-      <Grid item md={2} sm={3} xs={12}>
-        <CustomSelect data={ORDERS_STATUS} fullWidth />
+      <Grid item md={2} sm={4} xs={12}>
+        <CustomSelect
+          data={ORDERS_DELIVERY_METHOD}
+          name="deliveryMethod"
+          value={filterData.deliveryMethod}
+          onChange={changeHandler}
+          fullWidth
+          label="طريقة التوصيل"
+        />
       </Grid>
-      <Grid item md={2} sm={3} xs={12}>
+      <Grid item md={2} sm={4} xs={12}>
+        <CustomSelect
+          data={ORDERS_STATUS}
+          name="orderStatus"
+          value={filterData.orderStatus}
+          onChange={changeHandler}
+          fullWidth
+          label="حالة الطلب"
+        />
+      </Grid>
+      <Grid item md={2} sm={4} xs={12}>
         <CustomInput
           label="من تاريخ"
           type="date"
+          name="startDate"
+          value={filterData.startDate}
+          onChange={changeHandler}
           InputLabelProps={{
             shrink: true
           }}
         />
       </Grid>
-      <Grid item md={2} sm={3} xs={12}>
+      <Grid item md={2} sm={4} xs={12}>
         <CustomInput
           label="الى تاريخ"
           type="date"
+          name="endDate"
+          value={filterData.endDate}
+          onChange={changeHandler}
           InputLabelProps={{
             shrink: true
           }}
@@ -59,7 +101,8 @@ const OrdersFilterControls = () => {
         justify="space-evenly"
         alignItems="center"
         xs={12}
-        md={3}
+        sm={4}
+        md={2}
       >
         <CustomButton size="small" icon={<SearchIcon />}>
           ابحث
