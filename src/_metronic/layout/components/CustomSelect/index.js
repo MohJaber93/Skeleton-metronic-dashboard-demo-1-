a73e3af0c6e6da1cc5b4ba061d2ebad8a37ecfbc
Mohammed Jaber <mohjbr.dev@gmail.com>
2021-04-03 23:09:31 +0300

    adjust select component

--- src/_metronic/layout/components/CustomSelect/index.js
+++ src/_metronic/layout/components/CustomSelect/index.js
@@ -3,16 +3,9 @@ import { MenuItem, TextField } from "@material-ui/core";
 
 const CustomSelect = ({ data, ...rest }) => {
   return (
-    <TextField
-      variant="outlined"
-      size="small"
-      select
-      defaultValue="All"
-      {...rest}
-    >
-      <MenuItem value="All">كل الطلبات</MenuItem>
+    <TextField variant="outlined" size="small" select {...rest}>
       {Object.keys(data).map(status => (
-        <MenuItem key={status} value={status}>
+        <MenuItem key={status} value={data[status]}>
           {data[status]}
         </MenuItem>
       ))}
