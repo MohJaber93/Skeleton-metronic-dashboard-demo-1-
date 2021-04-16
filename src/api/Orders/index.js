1d2c8b3ee6de5b96dd650d1fac934ebb717c18f3
Mohammed Jaber <mohjbr.dev@gmail.com>
2021-04-16 23:34:40 +0300

    remove unused functions

--- src/api/Orders/index.js
+++ src/api/Orders/index.js
@@ -1,170 +1,17 @@
 import { API_COMMON_STATUS, getUnknownStatusError } from "helpers/api-helper";
 import axios from "axios";
 
-export const getOrders = async () => {
+export const getOrders = async query => {
+  let url = "/orders/all";
+  if (query) {
+    url = `/orders/all?${query}`;
+  }
   try {
-    const response = await axios.get(`/orders/all`, {
+    const response = await axios.get(url, {
       headers: {
         Authorization: `Bearer ${localStorage.getItem("token")}`
       }
     });
-    let data = {};
-    switch (response.status) {
-      case API_COMMON_STATUS.SUCCESS:
-        data = {
-          responseStatus: API_COMMON_STATUS.SUCCESS,
-          ...response.data
-        };
-        break;
-      case API_COMMON_STATUS.UNAUTHORIZED:
-        data = {
-          responseStatus: API_COMMON_STATUS.UNAUTHORIZED,
-          message: response.data.error
-        };
-        break;
-      case API_COMMON_STATUS.ERROR:
-        data = {
-          responseStatus: API_COMMON_STATUS.ERROR,
-          message: response.data.message
-        };
-        break;
-      default:
-        data = getUnknownStatusError();
-    }
-    return data;
-  } catch (error) {
-    console.log(error, error.message, error.name);
-    console.error(error);
-  }
-};
-
-export const filterByOrderNumber = async number => {
-  try {
-    const response = await axios.get(
-      `/orders/search-number?order_number=${number}`,
-      {
-        headers: {
-          Authorization: `Bearer ${localStorage.getItem("token")}`
-        }
-      }
-    );
-    let data = {};
-    switch (response.status) {
-      case API_COMMON_STATUS.SUCCESS:
-        data = {
-          responseStatus: API_COMMON_STATUS.SUCCESS,
-          ...response.data
-        };
-        break;
-      case API_COMMON_STATUS.UNAUTHORIZED:
-        data = {
-          responseStatus: API_COMMON_STATUS.UNAUTHORIZED,
-          message: response.data.error
-        };
-        break;
-      case API_COMMON_STATUS.ERROR:
-        data = {
-          responseStatus: API_COMMON_STATUS.ERROR,
-          message: response.data.message
-        };
-        break;
-      default:
-        data = getUnknownStatusError();
-    }
-    return data;
-  } catch (error) {
-    console.log(error, error.message, error.name);
-    console.error(error);
-  }
-};
-
-export const filterByOrderStatus = async status => {
-  try {
-    const response = await axios.get(`/orders/filter-status?status=${status}`, {
-      headers: {
-        Authorization: `Bearer ${localStorage.getItem("token")}`
-      }
-    });
-    let data = {};
-    switch (response.status) {
-      case API_COMMON_STATUS.SUCCESS:
-        data = {
-          responseStatus: API_COMMON_STATUS.SUCCESS,
-          ...response.data
-        };
-        break;
-      case API_COMMON_STATUS.UNAUTHORIZED:
-        data = {
-          responseStatus: API_COMMON_STATUS.UNAUTHORIZED,
-          message: response.data.error
-        };
-        break;
-      case API_COMMON_STATUS.ERROR:
-        data = {
-          responseStatus: API_COMMON_STATUS.ERROR,
-          message: response.data.message
-        };
-        break;
-      default:
-        data = getUnknownStatusError();
-    }
-    return data;
-  } catch (error) {
-    console.log(error, error.message, error.name);
-    console.error(error);
-  }
-};
-
-export const filterByDeliveryMethod = async deliveryMethod => {
-  try {
-    const response = await axios.get(
-      `/orders/filter-delivery-method?delivery_method=${deliveryMethod}`,
-      {
-        headers: {
-          Authorization: `Bearer ${localStorage.getItem("token")}`
-        }
-      }
-    );
-    let data = {};
-    switch (response.status) {
-      case API_COMMON_STATUS.SUCCESS:
-        data = {
-          responseStatus: API_COMMON_STATUS.SUCCESS,
-          ...response.data
-        };
-        break;
-      case API_COMMON_STATUS.UNAUTHORIZED:
-        data = {
-          responseStatus: API_COMMON_STATUS.UNAUTHORIZED,
-          message: response.data.error
-        };
-        break;
-      case API_COMMON_STATUS.ERROR:
-        data = {
-          responseStatus: API_COMMON_STATUS.ERROR,
-          message: response.data.message
-        };
-        break;
-      default:
-        data = getUnknownStatusError();
-    }
-    return data;
-  } catch (error) {
-    console.log(error, error.message, error.name);
-    console.error(error);
-  }
-};
-
-export const filterByOrderDate = async payload => {
-  try {
-    const response = await axios.get(
-      `/orders/filter-date?start=${payload.startDate}&end=${payload.endDate}`,
-      {
-        headers: {
-          Authorization: `Bearer ${localStorage.getItem("token")}`
-        }
-      }
-    );
     let data = {};
     switch (response.status) {
       case API_COMMON_STATUS.SUCCESS:
