a28d9c721efdb9869173820e3aebfc35177f95a3
Mohammed Jaber <mohjbr.dev@gmail.com>
2021-04-17 00:08:23 +0300

    add update status api function

--- src/api/Orders/index.js
+++ src/api/Orders/index.js
@@ -41,3 +41,44 @@ export const getOrders = async query => {
     console.error(error);
   }
 };
+
+export const updateOrderStatus = async payload => {
+  try {
+    const response = await axios.post(
+      `/orders/update-status/${payload.orderId}`,
+      { status: payload.updatedValue },
+      {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`
+        }
+      }
+    );
+    let data = {};
+    switch (response.status) {
+      case API_COMMON_STATUS.SUCCESS:
+        data = {
+          responseStatus: API_COMMON_STATUS.SUCCESS,
+          ...response.data
+        };
+        break;
+      case API_COMMON_STATUS.UNAUTHORIZED:
+        data = {
+          responseStatus: API_COMMON_STATUS.UNAUTHORIZED,
+          message: response.data.error
+        };
+        break;
+      case API_COMMON_STATUS.ERROR:
+        data = {
+          responseStatus: API_COMMON_STATUS.ERROR,
+          message: response.data.message
+        };
+        break;
+      default:
+        data = getUnknownStatusError();
+    }
+    return data;
+  } catch (error) {
+    console.log(error, error.message, error.name);
+    console.error(error);
+  }
+};
