e2719fef2266154e71baf7549332af2950114713
Mohammed Jaber <mohjbr.dev@gmail.com>
2021-04-17 17:55:38 +0300

    change OrdersCards to be usable component

---
+++ src/_metronic/layout/components/RoundedCards/index.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { Typography, Grid, CircularProgress } from "@material-ui/core";
+import CustomCard from "../CustomCard";
+import { useStyles } from "./style";
+
+const OrdersCards = ({ data }) => {
+  const classes = useStyles();
+
+  const renderCardValue = value =>
+    value || value === 0 ? value : <CircularProgress size={30} />;
+
+  return (
+    <CustomCard styleClass={classes.cardsSection}>
+      <CustomCard>
+        <CustomCard>
+          <Grid container>
+            {data.map(card => (
+              <Grid
+                item
+                container
+                direction="column"
+                justify="space-between"
+                alignItems="center"
+                xs={4}
+                md={2}
+                key={card.id}
+              >
+                <CustomCard
+                  className={`${classes.card} ${classes[card.className]}`}
+                >
+                  <Typography variant="h5">
+                    {renderCardValue(card.value)}
+                  </Typography>
+                </CustomCard>
+                <Typography variant="h6">{card.title}</Typography>
+              </Grid>
+            ))}
+          </Grid>
+        </CustomCard>
+      </CustomCard>
+    </CustomCard>
+  );
+};
+
+export default OrdersCards;
