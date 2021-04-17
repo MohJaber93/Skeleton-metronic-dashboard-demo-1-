82fe25d86c0c4677de28c1952b2b731c43b4fa23
Mohammed Jaber <mohjbr.dev@gmail.com>
2021-04-17 20:43:26 +0300

    add ordersHeadCells & usersHeadCells constants

--- src/app/constants/index.js
+++ src/app/constants/index.js
@@ -23,3 +23,94 @@ export const USERS_TYPE = {
   seller: "بائع",
   buyer: "مشتري"
 };
+
+export const ordersHeadCells = [
+  {
+    id: "number",
+    numeric: false,
+    disablePadding: true,
+    label: "رقم الطلب"
+  },
+  {
+    id: "userName",
+    numeric: false,
+    disablePadding: false,
+    label: "اسم المستخدم"
+  },
+  {
+    id: "sellerName",
+    numeric: true,
+    disablePadding: false,
+    label: "اسم البائع"
+  },
+  {
+    id: "orderDate",
+    numeric: true,
+    disablePadding: false,
+    label: "تاريخ الطلب"
+  },
+  {
+    id: "orderTotal",
+    numeric: true,
+    disablePadding: false,
+    label: "قيمة الطلب"
+  },
+  {
+    id: "deliveryMethod",
+    numeric: true,
+    disablePadding: false,
+    label: "طريقة التوصيل"
+  },
+  {
+    id: "orderStatus",
+    numeric: true,
+    disablePadding: false,
+    label: "حالة الطلب"
+  },
+  { id: "fees", numeric: true, disablePadding: false, label: "المصاريف" }
+];
+
+export const usersHeadCells = [
+  {
+    id: "number",
+    numeric: false,
+    disablePadding: true,
+    label: "#"
+  },
+  {
+    id: "userName",
+    numeric: false,
+    disablePadding: false,
+    label: "اسم المستخدم"
+  },
+  {
+    id: "accountName",
+    numeric: true,
+    disablePadding: false,
+    label: "اسم الحساب"
+  },
+  {
+    id: "userEmail",
+    numeric: true,
+    disablePadding: false,
+    label: "الايميل"
+  },
+  {
+    id: "phoneNumber",
+    numeric: true,
+    disablePadding: false,
+    label: "رقم الهاتف"
+  },
+  {
+    id: "userType",
+    numeric: true,
+    disablePadding: false,
+    label: "نوع المستخدم"
+  },
+  {
+    id: "actions",
+    numeric: true,
+    disablePadding: false,
+    label: "اجراءات"
+  }
+];
