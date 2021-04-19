9dcbf699d95eedcd1e354d3730e6c1505a8410e1
Mohammed Jaber <mohjbr.dev@gmail.com>
2021-04-19 23:34:54 +0300

    add users apis functions

--- src/api/Users/index.js
+++ src/api/Users/index.js
@@ -38,3 +38,123 @@ export const getUsers = async () => {
     console.error(error);
   }
 };
+
+export const addNewUser = async userData => {
+  try {
+    const response = await axios.post(`/users/add-user`, userData, {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("token")}`
+      }
+    });
+    let data = {};
+    switch (response.status) {
+      case API_COMMON_STATUS.SUCCESS:
+        data = {
+          responseStatus: API_COMMON_STATUS.SUCCESS,
+          ...response.data
+        };
+        break;
+      case API_COMMON_STATUS.UNAUTHORIZED:
+        data = {
+          responseStatus: API_COMMON_STATUS.UNAUTHORIZED,
+          message: response.data.error
+        };
+        break;
+      case API_COMMON_STATUS.ERROR:
+        data = {
+          responseStatus: API_COMMON_STATUS.ERROR,
+          message: response.data.message
+        };
+        break;
+      default:
+        data = getUnknownStatusError();
+    }
+    return data;
+  } catch (error) {
+    console.log(error, error.message, error.name);
+    console.error(error);
+  }
+};
+
+export const updateUserData = async payload => {
+  try {
+    const response = await axios.put(
+      `/users/update-user/${payload.userId}`,
+      payload.userData,
+      {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`
+        }
+      }
+    );
+    let data = {};
+    switch (response.status) {
+      case API_COMMON_STATUS.SUCCESS:
+        data = {
+          responseStatus: API_COMMON_STATUS.SUCCESS,
+          ...response.data
+        };
+        break;
+      case API_COMMON_STATUS.UNAUTHORIZED:
+        data = {
+          responseStatus: API_COMMON_STATUS.UNAUTHORIZED,
+          message: response.data.error
+        };
+        break;
+      case API_COMMON_STATUS.ERROR:
+        data = {
+          responseStatus: API_COMMON_STATUS.ERROR,
+          message: response.data.message
+        };
+        break;
+      default:
+        data = getUnknownStatusError();
+    }
+    return data;
+  } catch (error) {
+    console.log(error, error.message, error.name);
+    console.error(error);
+  }
+};
+
+export const blockUser = async userId => {
+  try {
+    const response = await axios.post(
+      `/users/block-user/${userId}`,
+      {},
+      {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`
+        }
+      }
+    );
+    console.log("test users data", response);
+    let data = {};
+    switch (response.status) {
+      case API_COMMON_STATUS.SUCCESS:
+        data = {
+          responseStatus: API_COMMON_STATUS.SUCCESS,
+          ...response.data
+        };
+        break;
+      case API_COMMON_STATUS.UNAUTHORIZED:
+        data = {
+          responseStatus: API_COMMON_STATUS.UNAUTHORIZED,
+          message: response.data.error
+        };
+        break;
+      case API_COMMON_STATUS.ERROR:
+        data = {
+          responseStatus: API_COMMON_STATUS.ERROR,
+          message: response.data.message
+        };
+        break;
+      default:
+        data = getUnknownStatusError();
+    }
+    return data;
+  } catch (error) {
+    console.log(error, error.message, error.name);
+    console.error(error);
+  }
+};
