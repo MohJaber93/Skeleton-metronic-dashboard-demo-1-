072900c81392c946194db2fba8d48a4b85a21520
Mohammed Jaber <mohjbr.dev@gmail.com>
2021-04-19 23:34:26 +0300

    add block logic and update user data dialog to user actions component

--- src/_metronic/layout/components/UsersTableActions/index.js
+++ src/_metronic/layout/components/UsersTableActions/index.js
@@ -1,33 +1,70 @@
 import React from "react";
 import { Tooltip, IconButton } from "@material-ui/core";
-import DeleteIcon from "@material-ui/icons/Delete";
 import BlockIcon from "@material-ui/icons/Block";
 import UpdateIcon from "@material-ui/icons/Update";
+import CheckCircleOutlineIcon from "@material-ui/icons/CheckCircleOutline";
+import UserFormDialog from "../UserFormDialog";
+import { blockUser } from "api/Users";
+import { API_COMMON_STATUS } from "helpers/api-helper";
 
-const UsersTableActions = () => {
+const UsersTableActions = ({ user, updateSnackbarState }) => {
+  const [openUserDialog, setOpenUserDialog] = React.useState(false);
+  const [isBlocked, setIsBlocked] = React.useState(user.isBlocked);
+  const userId = user.id;
+
+  const handleClickOpenUserDialog = () => {
+    setOpenUserDialog(true);
+  };
+
+  const handleCloseUserDialog = () => {
+    setOpenUserDialog(false);
+  };
+
+  const blockUserHandler = () => {
+    blockUser(userId)
+      .then(response => {
+        console.log("test users data", response);
+        if (response.responseStatus === API_COMMON_STATUS.SUCCESS) {
+          setIsBlocked(response.isBlocked);
+          updateSnackbarState({
+            open: true,
+            message: response.message,
+            variant: "success"
+          });
+        } else {
+          updateSnackbarState({
+            open: true,
+            message: response.message,
+            variant: "error"
+          });
+        }
+      })
+      .catch(error => {
+        console.log(error);
+        console.error(error);
+      });
+  };
+
   return (
     <>
+      {openUserDialog && (
+        <UserFormDialog
+          open={openUserDialog}
+          handleClose={handleCloseUserDialog}
+          userData={user}
+        />
+      )}
       <Tooltip title="تحديث بيانات المستخدم">
-        <IconButton
-          color="primary"
-          //   onClick={updateTableData}
-        >
+        <IconButton color="primary" onClick={handleClickOpenUserDialog}>
           <UpdateIcon />
         </IconButton>
       </Tooltip>
-      <Tooltip title="حجب المتسخدم">
+      <Tooltip title={isBlocked ? "رفع الحجب" : "حجب المتسخدم"}>
         <IconButton
-        //   onClick={updateTableData}
+          color={isBlocked ? "primary" : "secondary"}
+          onClick={blockUserHandler}
         >
-          <BlockIcon />
-        </IconButton>
-      </Tooltip>
-      <Tooltip title="حذف المتسخدم">
-        <IconButton
-          color="secondary"
-          //   onClick={updateTableData}
-        >
-          <DeleteIcon />
+          {isBlocked ? <CheckCircleOutlineIcon /> : <BlockIcon />}
         </IconButton>
       </Tooltip>
     </>
