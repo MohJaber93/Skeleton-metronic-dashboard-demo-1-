52c81282ee4f5b029e9922790bd9b5510d2c7464
Mohammed Jaber <mohjbr.dev@gmail.com>
2021-04-21 00:42:02 +0300

    remove unused import

--- src/_metronic/layout/components/LayoutInit.js
+++ src/_metronic/layout/components/LayoutInit.js
@@ -1,5 +1,5 @@
-import React, {useLayoutEffect} from "react";
-import {KTUtil} from "../../_assets/js/components/util";
+import React, { useLayoutEffect } from "react";
+import { KTUtil } from "../../_assets/js/components/util";
 import KTLayoutHeader from "../../_assets/js/layout/base/header";
 import KTLayoutHeaderMenu from "../../_assets/js/layout/base/header-menu";
 import KTLayoutHeaderTopbar from "../../_assets/js/layout/base/header-topbar";
@@ -14,57 +14,54 @@ import KTLayoutScrolltop from "../../_assets/js/layout/extended/scrolltop";
 import KTLayoutStickyCard from "../../_assets/js/layout/base/sticky-card";
 import KTLayoutStretchedCard from "../../_assets/js/layout/base/stretched-card";
 import KTLayoutQuickPanel from "../../_assets/js/layout/extended/quick-panel";
-import KTLayoutQuickUser from "../../_assets/js/layout/extended/quick-user";
 
 export function LayoutInit() {
   useLayoutEffect(() => {
     // Initialization
-    KTUtil.ready(function () {
+    KTUtil.ready(function() {
       ////////////////////////////////////////////////////
       // Layout Base Partials(mandatory for core layout)//
       ////////////////////////////////////////////////////
       // Init Desktop & Mobile Headers
-      KTLayoutHeader.init('kt_header', 'kt_header_mobile');
+      KTLayoutHeader.init("kt_header", "kt_header_mobile");
 
       // Init Header Menu
-      KTLayoutHeaderMenu.init('kt_header_menu', 'kt_header_menu_wrapper');
+      KTLayoutHeaderMenu.init("kt_header_menu", "kt_header_menu_wrapper");
       // Init Header Topbar For Mobile Mode
-      KTLayoutHeaderTopbar.init('kt_header_mobile_topbar_toggle');
+      KTLayoutHeaderTopbar.init("kt_header_mobile_topbar_toggle");
       // Init Brand Panel For Logo
-      KTLayoutBrand.init('kt_brand');
+      KTLayoutBrand.init("kt_brand");
       // Init Aside
-      KTLayoutAside.init('kt_aside');
+      KTLayoutAside.init("kt_aside");
 
       // Init Aside Menu Toggle
-      KTLayoutAsideToggle.init('kt_aside_toggle');
+      KTLayoutAsideToggle.init("kt_aside_toggle");
       //
       // Init Aside Menu
-      KTLayoutAsideMenu.init('kt_aside_menu');
+      KTLayoutAsideMenu.init("kt_aside_menu");
 
       // Init Content
-      KTLayoutContent.init('kt_content');
+      KTLayoutContent.init("kt_content");
 
       // Init Footer
-      KTLayoutFooter.init('kt_footer');
-
+      KTLayoutFooter.init("kt_footer");
 
       //////////////////////////////////////////////
       // Layout Extended Partials(optional to use)//
       //////////////////////////////////////////////
-      KTLayoutSubheader.init('kt_subheader');
+      KTLayoutSubheader.init("kt_subheader");
 
       // Init Scrolltop
-      KTLayoutScrolltop.init('kt_scrolltop');
+      KTLayoutScrolltop.init("kt_scrolltop");
 
       // Init Sticky Card
-      KTLayoutStickyCard.init('kt_page_sticky_card');
+      KTLayoutStickyCard.init("kt_page_sticky_card");
 
       // Init Stretched Card
-      KTLayoutStretchedCard.init('kt_page_stretched_card');
+      KTLayoutStretchedCard.init("kt_page_stretched_card");
 
       // Init Quick Offcanvas Panel
-      KTLayoutQuickPanel.init('kt_quick_panel');
-
+      KTLayoutQuickPanel.init("kt_quick_panel");
     });
   }, []);
   return <></>;
