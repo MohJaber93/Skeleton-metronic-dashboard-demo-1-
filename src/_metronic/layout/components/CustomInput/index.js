a088ec26fcd66325eb960672e5328bc4f5588065
Mohammed Jaber <mohjbr.dev@gmail.com>
2021-06-25 00:35:00 +0300

    customize input component

--- src/_metronic/layout/components/CustomInput/index.js
+++ src/_metronic/layout/components/CustomInput/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { TextField } from "@material-ui/core";
 
-const CustomInput = ({ ...rest }) => {
-  return <TextField fullWidth variant="outlined" size="small" {...rest} />;
+const CustomInput = ({ size = "small", ...rest }) => {
+  return <TextField fullWidth variant="outlined" size={size} {...rest} />;
 };
 
 export default CustomInput;
